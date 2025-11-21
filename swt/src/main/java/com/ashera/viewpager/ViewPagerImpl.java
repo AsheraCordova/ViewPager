37efe66fb4af92ce1e78ae88394042e2ecc2aa57
Ashera Cordova <ashera.cordova@gmail.com>
2025-11-21 15:14:05 +0530

    Added licensing to all files + ratingbar support.

--- swt/src/main/java/com/ashera/viewpager/ViewPagerImpl.java
+++ swt/src/main/java/com/ashera/viewpager/ViewPagerImpl.java
@@ -1,3 +1,18 @@
+//start - license
+/*
+ * Copyright (c) 2025 Ashera Cordova
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+//end - license
 package com.ashera.viewpager;
 // start - imports
 import java.util.*;
@@ -1005,7 +1020,7 @@ public java.util.Map<String, Object> getOnPageScrollStateChangedEventObj(int sta
 	private void nativeCreate(Map<String, Object> params) {
 		viewPager.setAdapter(new CustomPagerAdapter());
 		pane = new org.eclipse.swt.widgets.Composite((org.eclipse.swt.widgets.Composite)ViewImpl.getParent(this), getStyle(params, fragment));
-        ((org.eclipse.swt.widgets.Composite)pane).setLayout(new org.eclipse.nebula.widgets.layout.AbsoluteLayout());
+        ((org.eclipse.swt.widgets.Composite)pane).setLayout(new com.ashera.common.AbsoluteLayout());
         
         addListeners();
 	}
